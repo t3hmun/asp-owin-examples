6a0103ba400c0c22a481e697b209c1dc0dc3dd32
t3hmun <git@t3hmun.com>
2017-03-28 18:27:33 +0100

    Add project files.

---
+++ asp-owin-examples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asp_owin_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
